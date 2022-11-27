b46f0c8e1386192ff975757d767f9d44a034b963
yadav-avnish <avnish@ineuron.ai>
2022-11-27 15:26:50 +0530

    updated the requirements.txt

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
+
